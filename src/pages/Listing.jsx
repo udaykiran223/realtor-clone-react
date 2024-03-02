20f8eb99e9e134965d69156a2dc88b8b088a52d7
Udaykiranakk <144661563+Udaykiranakk@users.noreply.github.com>
2024-03-02 16:11:33 +0530

    fix leaflet popup

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -160,9 +160,7 @@ export default function Listing() {
             <Marker
               position={[listing.geolocation.lat, listing.geolocation.lng]}
             >
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
+              <Popup>{listing.address}</Popup>
             </Marker>
           </MapContainer>
         </div>
